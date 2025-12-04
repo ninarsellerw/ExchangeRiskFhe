317fcf1cf5a440ea2dcf5d21c56a50cc39151043
ninarsellerw <RosenfieldCastellion249@outlook.com>
2025-12-05 01:14:32 +0800

    add backend-dist-styles.less, index.ts, styles-inventory-common.js

---
+++ frontend/web/src/mutations.py
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\mutations.py
+// Generated at: 2025-10-17T08:22:49.211Z
---
+++ styles-inventory-common.js
@@ -0,0 +1,3 @@
+import axios from 'axios';
+
+export const usepayment-database = () => { return {}; };
